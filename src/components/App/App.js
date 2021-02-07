859e46db986fbe22572464d7bceee4e2789731e5
TOR968 <tor968@yahoo.com>
2021-02-07 17:30:11 +0200

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,59 @@
-import ContactForm from '../ContactForm';
-import Filter from '../Filter';
-import ContactList from '../ContactList';
+import React, { Suspense, lazy, useEffect } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import { Container, MainTitle, ContactTitle } from './AppStyles';
+import AppBar from '../AppBar';
+import PreLoader from '../PreLoader';
+import routes from '../../routes';
+import PrivateRoute from '../PrivateRoute';
+import PublicRoute from '../PublicRoute';
+import { getCurrentUser } from '../../redux/auth/auth-operations';
+import { fetchUserContact } from '../../redux/phonebook/phonebook-operations';
+
+const HomePage = lazy(() =>
+  import('../../pages/HomePage' /* webpackChunkName: "home" */),
+);
+const ContactsPage = lazy(() =>
+  import('../../pages/ContactsPage' /* webpackChunkName: "ContactsPage" */),
+);
+const LoginPage = lazy(() =>
+  import('../../pages/LoginPage' /* webpackChunkName: "LoginPage" */),
+);
+const RegisterPage = lazy(() =>
+  import('../../pages/RegisterPage' /* webpackChunkName: "RegisterPage" */),
+);
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCurrentUser());
+    dispatch(fetchUserContact());
+  }, [dispatch]);
+
   return (
-    <Container>
-      <MainTitle>Phonebook</MainTitle>
-      <ContactForm />
-      <ContactTitle>Contacts</ContactTitle>
-      <Filter />
-      <ContactList />
-    </Container>
+    <>
+      <AppBar />
+      <main>
+        <Suspense fallback={<PreLoader sizePreloader="200px" />}>
+          <Switch>
+            <Route exact path={routes.home} component={HomePage} />
+            <PrivateRoute path={routes.contactsPage} component={ContactsPage} />
+            <PublicRoute
+              path={routes.loginPage}
+              restricted
+              component={LoginPage}
+            />
+            <PublicRoute
+              path={routes.registerPage}
+              restricted
+              component={RegisterPage}
+            />
+            <Redirect to={routes.home} />
+          </Switch>
+        </Suspense>
+      </main>
+    </>
   );
 };
 

805f702ff16ed92c3895a6a0fe1743c70a900bb4
TOR968 <tor968@yahoo.com>
2021-02-07 17:28:21 +0200

    home Page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,6 @@
+import Title from './HomePage.style';
+
+const HomePage = () => {
+  return <Title>Welcome to PhoneBook App!</Title>;
+};
+export default HomePage;
---
+++ src/pages/HomePage/HomePage.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Title = styled.div`
+  font-size: 36px;
+  color: var(--secondColor);
+  text-align: center;
+  margin-top: 20px;
+`;
+
+export default Title;
